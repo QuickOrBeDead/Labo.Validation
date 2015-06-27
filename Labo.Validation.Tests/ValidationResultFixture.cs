7a74ac5717170e62e24425f8fb22b952f2fd4c45
Bora Akgun <boraakgn@gmail.com>
2015-06-28 02:09:57 +0300

    added ValidationResult tests

---
+++ Labo.Validation.Tests/ValidationResultFixture.cs
@@ -0,0 +1,32 @@
+﻿namespace Labo.Validation.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ValidationResultFixture
+    {
+        [Test]
+        public void IsValidMustReturnFalseWhenErrorCountIsBiggerThan0()
+        {
+            ValidationResult validationResult = new ValidationResult();
+            validationResult.Errors = new ValidationErrorCollection { new ValidationError() };
+
+            Assert.IsFalse(validationResult.IsValid);
+        }
+
+        [Test]
+        public void IsValidMustReturnTrueWhenErrorCountIs0()
+        {
+            ValidationResult validationResult = new ValidationResult();
+            validationResult.Errors = new ValidationErrorCollection();
+
+            Assert.IsTrue(validationResult.IsValid);
+        }
+
+        [Test]
+        public void IsValidMustReturnTrueForEmptyValidationResult()
+        {
+            Assert.IsTrue(ValidationResult.Empty().IsValid);
+        }
+    }
+}
