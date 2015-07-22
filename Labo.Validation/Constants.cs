be5c79719c3f3d1d802f21b325656ece5132dd60
Bora Akgun <boraakgn@gmail.com>
2015-07-22 07:12:59 +0300

    .

---
+++ Labo.Validation/Constants.cs
@@ -0,0 +1,132 @@
+﻿namespace Labo.Validation
+{
+    /// <summary>
+    /// The constants class.
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// The validation message resource names.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1034:NestedTypesShouldNotBeVisible")]
+        public static class ValidationMessageResourceNames
+        {
+            /// <summary>
+            /// The equal to validation message
+            /// </summary>
+            public const string EQUAL_TO_VALIDATION_MESSAGE = "EqualToValidationMessage";
+
+            /// <summary>
+            /// The not empty validation message
+            /// </summary>
+            public const string NOT_EMPTY_VALIDATION_MESSAGE = "NotEmptyValidationMessage";
+
+            /// <summary>
+            /// The not equal to validation message
+            /// </summary>
+            public const string NOT_EQUAL_TO_VALIDATION_MESSAGE = "NotEqualToValidationMessage";
+
+            /// <summary>
+            /// The not null validation message
+            /// </summary>
+            public const string NOT_NULL_VALIDATION_MESSAGE = "NotNullValidationMessage";
+
+            /// <summary>
+            /// The email validation message
+            /// </summary>
+            public const string EMAIL_VALIDATION_MESSAGE = "EMailValidationMessage";
+
+            /// <summary>
+            /// The phone number validation message
+            /// </summary>
+            public const string PHONE_NUMBER_VALIDATION_MESSAGE = "PhoneNumberValidationMessage";
+
+            /// <summary>
+            /// The predicate validation message
+            /// </summary>
+            public const string PREDICATE_VALIDATION_MESSAGE = "PredicateValidationMessage";
+
+            /// <summary>
+            /// The regex validation message
+            /// </summary>
+            public const string REGEX_VALIDATION_MESSAGE = "RegexValidationMessage";
+
+            /// <summary>
+            /// The url validation message
+            /// </summary>
+            public const string URL_VALIDATION_MESSAGE = "UrlValidationMessage";
+
+            /// <summary>
+            /// The greater than validation message
+            /// </summary>
+            public const string GREATER_THAN_VALIDATION_MESSAGE = "GreaterThanValidationMessage";
+
+            /// <summary>
+            /// The greater than or equal to validation message
+            /// </summary>
+            public const string GREATER_THAN_OR_EQUAL_TO_VALIDATION_MESSAGE = "GreaterThanOrEqualToValidationMessage";
+
+            /// <summary>
+            /// The less than validation message
+            /// </summary>
+            public const string LESS_THAN_VALIDATION_MESSAGE = "LessThanValidationMessage";
+
+            /// <summary>
+            /// The less than or equal to validation message
+            /// </summary>
+            public const string LESS_THAN_OR_EQUAL_TO_VALIDATION_MESSAGE = "LessThanOrEqualToValidationMessage";
+
+            /// <summary>
+            /// The credit card validation message
+            /// </summary>
+            public const string CREDIT_CARD_VALIDATION_MESSAGE = "CreditCardValidationMessage";
+
+            /// <summary>
+            /// The between validation message
+            /// </summary>
+            public const string BETWEEN_VALIDATION_MESSAGE = "BetweenValidationMessage";
+
+            /// <summary>
+            /// The length validation message
+            /// </summary>
+            public const string LENGTH_VALIDATION_MESSAGE = "LenghtValidationMessage";
+        }
+
+        /// <summary>
+        /// The validation message parameter names.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1034:NestedTypesShouldNotBeVisible")]
+        public static class ValidationMessageParameterNames
+        {
+            /// <summary>
+            /// The value name
+            /// </summary>
+            public const string VALUE_NAME = "ValueName";
+
+            /// <summary>
+            /// The value to compare
+            /// </summary>
+            public const string VALUE_TO_COMPARE = "ValueToCompare";
+
+            /// <summary>
+            /// The from value
+            /// </summary>
+            public const string FROM_VALUE = "FromValue";
+
+            /// <summary>
+            /// The to value
+            /// </summary>
+            public const string TO_VALUE = "ToValue";
+            
+            /// <summary>
+            /// The maximum
+            /// </summary>
+            public const string MAX = "Max";
+
+            /// <summary>
+            /// The minimum
+            /// </summary>
+            public const string MIN = "Min";
+        }
+    }
+}
