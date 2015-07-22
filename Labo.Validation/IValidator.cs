287ee815a7b458560ce6df40d7f30ba43bf51765
Bora Akgun <boraakgn@gmail.com>
2015-07-22 07:10:21 +0300

    added get validation message method

--- Labo.Validation/IValidator.cs
+++ Labo.Validation/IValidator.cs
@@ -9,7 +9,15 @@
         /// Determines whether the specified value is valid.
         /// </summary>
         /// <param name="value">The value.</param>
-        /// <returns><c>true</c> if the specified value is valid otherwise <c>false</c></returns>
+        /// <returns><c>true</c> if the specified value is valid, otherwise <c>false</c></returns>
         bool IsValid(object value);
+
+        /// <summary>
+        /// Gets the validation message.
+        /// </summary>
+        /// <param name="valueName">Name of the value.</param>
+        /// <param name="arguments">The arguments.</param>
+        /// <returns>The validation message</returns>
+        string GetValidationMessage(string valueName, params string[] arguments);
     }
 }
