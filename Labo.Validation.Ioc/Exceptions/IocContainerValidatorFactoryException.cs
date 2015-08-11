a39edcafd146a9060d92fae8ec379e0838ebfb1f
Bora Akgun <boraakgn@gmail.com>
2015-08-11 21:01:05 +0300

    added validation ioc project

---
+++ Labo.Validation.Ioc/Exceptions/IocContainerValidatorFactoryException.cs
@@ -0,0 +1,59 @@
+﻿namespace Labo.Validation.Ioc.Exceptions
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    using Labo.Common.Exceptions;
+
+    /// <summary>
+    /// The ioc container validation exception class.
+    /// </summary>
+    [Serializable]
+    public class IocContainerValidatorFactoryException : CoreLevelException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IocContainerValidatorFactoryException"/> class.
+        /// </summary>
+        public IocContainerValidatorFactoryException()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IocContainerValidatorFactoryException"/> class.
+        /// </summary>
+        /// <param name="innerException">The inner exception.</param>
+        public IocContainerValidatorFactoryException(Exception innerException)
+            : base(null, innerException)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IocContainerValidatorFactoryException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public IocContainerValidatorFactoryException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IocContainerValidatorFactoryException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="innerException">The inner exception.</param>
+        public IocContainerValidatorFactoryException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IocContainerValidatorFactoryException"/> class.
+        /// </summary>
+        /// <param name="serializationInfo">The serialization info.</param>
+        /// <param name="context">The context.</param>
+        protected IocContainerValidatorFactoryException(SerializationInfo serializationInfo, StreamingContext context)
+            : base(serializationInfo, context)
+        {
+        }
+    }
+}
---
+++ Labo.Validation.Ioc/IocContainerValidatorFactory.cs
@@ -0,0 +1,64 @@
+﻿namespace Labo.Validation.Ioc
+{
+    using System;
+    using System.Globalization;
+
+    ///// <summary>
+    ///// The ioc container valitation factory class.
+    ///// </summary>
+    //public sealed class IocContainerValidatorFactory : IValidatorFactory
+    //{
+    //    /// <summary>
+    //    /// The ioc container resolver
+    //    /// </summary>
+    //    private readonly IIocContainerResolver m_IocContainerResolver;
+
+    //    /// <summary>
+    //    /// Initializes a new instance of the <see cref="IocContainerValidatorFactory"/> class.
+    //    /// </summary>
+    //    /// <param name="iocContainerResolver">The ioc container resolver.</param>
+    //    public IocContainerValidatorFactory(IIocContainerResolver iocContainerResolver) 
+    //    {
+    //        if (iocContainerResolver == null)
+    //        {
+    //            throw new ArgumentNullException("iocContainerResolver");
+    //        }
+
+    //        m_IocContainerResolver = iocContainerResolver;
+    //    }
+
+    //    /// <summary>
+    //    /// Gets the validator for the specified entity type.
+    //    /// </summary>
+    //    /// <typeparam name="TEntity">The type of the entity.</typeparam>
+    //    /// <returns>The entity validator.</returns>
+    //    public IEntityValidator<TEntity> GetValidatorFor<TEntity>()
+    //    {
+    //        return (IEntityValidator<TEntity>)GetValidatorFor(typeof(TEntity));
+    //    }
+
+    //    /// <summary>
+    //    /// Gets the validator for the specified type.
+    //    /// </summary>
+    //    /// <param name="type">The type.</param>
+    //    /// <returns>The entity validator.</returns>
+    //    public IEntityValidator GetValidatorFor(Type type)
+    //    {
+    //        if (type == null)
+    //        {
+    //            throw new ArgumentNullException("type");
+    //        }
+
+    //        Type genericType = typeof(IEntityValidator<>).MakeGenericType(type);
+    //        IEntityValidator entityValidator = m_IocContainerResolver.GetInstanceOptionalByName(genericType, type.FullName) as IEntityValidator;
+
+    //        if (entityValidator == null)
+    //        {
+    //            IocContainerValidatorFactoryException iocContainerValidatorFactoryException = new IocContainerValidatorFactoryException(string.Format(CultureInfo.CurrentCulture, "Entity validator for type: '{0}'.", type.FullName));
+    //            throw iocContainerValidatorFactoryException;
+    //        }
+
+    //        return entityValidator;
+    //    }
+    //}
+}
