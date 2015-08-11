43dde3169c2908f49be88b104a1edc5c28d35565
Bora Akgun <boraakgn@gmail.com>
2015-08-11 21:01:31 +0300

    added validation ioc tests project

---
+++ Labo.Validation.Ioc.Tests/IocContainerValidatorFactoryFixture.cs
@@ -0,0 +1,42 @@
+﻿namespace Labo.Validation.Ioc.Tests
+{
+    using System;
+
+    using Labo.Validation.Ioc.Exceptions;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    //[TestFixture]
+    //public class IocContainerValidatorFactoryFixture
+    //{
+    //    [Test]
+    //    public void ConstructorThrowsArgumentNullExceptionWhenIIocContainerResolverIsNull()
+    //    {
+    //        Assert.Throws<ArgumentNullException>(() => new IocContainerValidatorFactory(null));
+    //    }
+
+    //    [Test]
+    //    public void GetValidatorForShouldReturnTheRegisteredEntityValidator()
+    //    {
+    //        IIocContainerResolver iocContainerResolver = Substitute.For<IIocContainerResolver>();
+    //        IEntityValidator<string> entityValidator = Substitute.For<IEntityValidator<string>>();
+    //        iocContainerResolver.GetInstanceOptionalByName(typeof(IEntityValidator<string>), typeof(string).FullName).Returns(entityValidator);
+
+    //        IocContainerValidatorFactory iocContainerValidatorFactory = new IocContainerValidatorFactory(iocContainerResolver);
+    //        Assert.AreSame(entityValidator, iocContainerValidatorFactory.GetValidatorFor<string>());
+    //        Assert.AreSame(entityValidator, iocContainerValidatorFactory.GetValidatorFor(typeof(string)));
+    //    }
+
+    //    [Test]
+    //    public void GetValidatorForThrowsExceptionWhenThereIsNoEntityValidatorRegisteredForTheSpecifiedEntity()
+    //    {
+    //        IIocContainerResolver iocContainerResolver = Substitute.For<IIocContainerResolver>();
+    //        iocContainerResolver.GetInstanceOptionalByName(typeof(IEntityValidator<string>), typeof(string).FullName).Returns(null);
+
+    //        IocContainerValidatorFactory iocContainerValidatorFactory = new IocContainerValidatorFactory(iocContainerResolver);
+    //        Assert.Throws<IocContainerValidatorFactoryException>(() => iocContainerValidatorFactory.GetValidatorFor<string>());
+    //    }
+    //}
+}
